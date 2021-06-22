47586f0c820fe38bf10a5f0182457b9ceaa21819
fe-olaf <frontend.olaf@gmail.com>
2021-06-22 23:25:04 +0900

    chore: ux 경험을 위한 테스트

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,14 @@ import styled from 'styled-components'
 
 import Banner from '$components/home/Banner'
 import RecommendUsersCarousel from '$components/home/RecommendUsersCarousel'
+import RecommendUsersCarousel2 from '$components/home/RecommendUsersCarousel2'
+import RecommendUsersCarousel3 from '$components/home/RecommendUsersCarousel3'
 
 const Container = styled.div`
   max-width: 960px;
   margin: 0 auto;
+
+  padding-bottom: 200px;
 `
 
 function Index() {
@@ -14,6 +18,8 @@ function Index() {
       <Banner />
       <Container>
         <RecommendUsersCarousel />
+        <RecommendUsersCarousel2 />
+        <RecommendUsersCarousel3 />
       </Container>
     </div>
   )
